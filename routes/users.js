76d6548a5d88d9ae602ba13b44d298258d5425bd
simi87 <simi.hstu@gmail.com>
2017-11-21 11:35:32 +0600

    Third

--- routes/users.js
+++ routes/users.js
@@ -28,4 +28,31 @@ router.post('/signup', function(req, res, next) {
     );
 });
 
+router.post('/login', function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.status(401).json({
+                err: info
+            });
+        }
+        req.logIn(user, function(err) {
+            if (err) {
+                return res.status(500).json({
+                    err: 'Could not log in user'
+                });
+            }
+
+            var token = Verify.getToken(user);
+            res.status(200).json({
+                status: 'Login successful!',
+                success: true,
+                token: token
+            });
+        });
+    })(req, res, next);
+});
+
 module.exports = router;
