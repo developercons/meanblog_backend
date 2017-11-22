06eacc714c98f9a5a34d8427dceb4058e5430079
simi87 <simi.hstu@gmail.com>
2017-11-22 17:29:01 +0600

    user registration

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var passport = require('passport');
-var authenticate = require('./authenticate');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -40,44 +40,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
-
-app.use(session({
-    name: 'session-id',
-    secret: '12345-67890-09876-54321',
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore()
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-function auth(req, res, next) {
-    console.log(req.session);
-    if (!req.session.user) {
-        var authHeader = req.headers.authorization;
-        if (!authHeader) {
-            var err = new Error('You are not authenticated!');
-            res.setHeader(' WWW-Authenticate', 'Basic');
-            err.status = 401;
-            next(err);
-            return;
-        }
-        var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-        var user = auth[0];
-        var pass = auth[1];
-        if (user == 'admin' && pass == 'password') {
-            res.cookie('user', 'admin', { signed: true });
-            next();
-        } else {
-            var err = new Error('You are not authenticated!');
-            error.status = 401;
-            next(err);
-        }
-
-    }
-}
+//app.use(cookieParser('12345-67890-09876-54321'));
 
 /*function auth(req, res, next) {
     console.log(req.user);
@@ -91,13 +54,41 @@ function auth(req, res, next) {
     }
 }*/
 
-app.use(auth);
+app.use(session({
+    name: 'session-id',
+    secret: '12345-67890-09876-54321',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', index);
 app.use('/users', users);
+
+function auth(req, res, next) {
+    console.log(req.session);
+
+    if (!req.session.user) {
+        var err = new Error('You are not authenticated!');
+        err.status = 403;
+        return next(err);
+    } else {
+        if (req.session.user === 'authenticated') {
+            next();
+        } else {
+            var err = new Error('You are not authenticated!');
+            err.status = 403;
+            return next(err);
+        }
+    }
+}
+
+app.use(auth);
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/articles', articleRouter);
 
 // catch 404 and forward to error handler
--- authenticate.js
+++ authenticate.js
@@ -1,8 +1,7 @@
-var passport = require('passport');
+/*var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
-//var config = require('./config');
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.deserializeUser(User.deserializeUser());*/
